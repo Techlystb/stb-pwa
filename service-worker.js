a9628b0ec7145575d0b9ebcb1e676e46ea514d28
Techlystb <techlystb@gmail.com>
2025-08-30 02:07:09 +0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'techlystb-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/firebase-config.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
+});
