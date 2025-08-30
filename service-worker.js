51535f0847023f0744fb5adc20eedfd3eb542e1e
Techlystb <techlystb@gmail.com>
2025-08-30 15:08:41 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'techlystb-cache-v1';
+
+
+const CACHE_NAME = "techlystb-cache-v1";
 const urlsToCache = [
   '/',
   '/index.html',
@@ -8,10 +10,33 @@ const urlsToCache = [
   '/firebase-config.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
+// Fetch from cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
+
+// Activate (clear old cache)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
