d4ddd854d9b58ddfedde361428bfc2c3a453dbca
Techlystb <techlystb@gmail.com>
2025-08-30 15:55:27 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,20 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => clients.claim());
-self.addEventListener("fetch", e => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'techlystb-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
